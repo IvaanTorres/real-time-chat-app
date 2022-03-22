1955770ad4d8b781f8849308012da38b5294ed10
Ivan Torres Garcia <ivantorresgarcia.02@gmail.com>
2022-03-22 23:35:04 +0100

    chore(#13): set ENV variable for socket server depending dev or prod environment
    I needed to find how to expose ENV variables outside the server

--- src/sockets/sockets.ts
+++ src/sockets/sockets.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
-
-const URL = 'http://localhost:3000' // TODO: Set ENV variable
+import { VITE_API_PORT, VITE_API_URL } from '../config'
+const URL = VITE_API_URL.includes('localhost') ? `${VITE_API_URL}:${VITE_API_PORT}` : `${VITE_API_URL}` // TODO: Set ENV variable
+// eslint-disable-next-line no-console
+console.log(URL)
 const socket = io(URL, { autoConnect: false })
 
 socket.onAny((event, ...args) => {
